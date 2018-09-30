5b9a26a9fe4b68fff87929257a3860fa0fab2e71
Alex Bukhmiller <alex23031996@yandex.ru>
2018-09-30 12:18:12 +0300

    added constant NO_SOLUTION

--- src/com/operations/matrix/GaussAlgorithm.java
+++ src/com/operations/matrix/GaussAlgorithm.java
@@ -6,7 +6,7 @@ import com.operations.matrix.interfaces.MatrixOperation;
 public class GaussAlgorithm extends BaseMatrix implements MatrixOperation {
 
     private final String MATRIX_CONSASTENCY = "Матрица несовместна!";
-
+    private final  String NO_SOLUTION = "Решений нет";
     public GaussAlgorithm(double[][] matrix) {
         matrixB = new double[matrix.length][];
         matrixA = new double[matrix.length][];
@@ -26,7 +26,7 @@ public class GaussAlgorithm extends BaseMatrix implements MatrixOperation {
         System.out.println(toString());
         if (matrixA[0][0] == 0)
             if (!swapFirstElement(matrixA, matrixB)) {
-                System.out.println("Решений нет!");
+                System.out.println(NO_SOLUTION);
                 return;
             }
         System.out.println(toString());
